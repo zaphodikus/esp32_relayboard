be40c18eca8474222b964e29677f1aa7383dca2e
Conrad Braam <conrad.braam@meteorinkjet.com>
2026-08-23 14:47:24 +0100

    added Arduino sketch

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# esp32_relayboard
-An esp32 relay board driver.
+#esp32\_relayboard
+
+##An esp32 relay board driver.
 5VDC / USB powered, including Python driver code
 
 This code is intended for and documents a simple ESP32 supermini with 8 isolated relays. How I used power over USB to drive up to 2 relays at a time maximum load in an 8 relay bank. This is a simple veroboard-able project because it is really just 8 pins for the relay driver transistors and 2 pins for power.
 
 Note, I'm driving LOW for the relays, it just happened that way, so you may need to read the ESP spec sheet and then also play with which relay board you do buy to get the driver signal right for you. I also have this power budget of only being able to drive 3 relays before the USB power budget (300mA?) seems to cause a USB bus power problem. Regardless that can be overcome by using a separate relay power supply source. With that one gotch abotu the drive direction out of the way I hope that's the dragons and other smoke sources covered.
+
+##web
+ESP32 supermini pinout notes [SoftCircuitry](https://softcircuitry.blogspot.com/2026/08/esp32-c3-supermini.html)
+Arduino [Testing ideas thread](https://forum.arduino.cc/t/how-to-create-a-simple-test-f-w-over-monitor/1456745)
+
---
+++ relaycontroller/relaycontroller.ino
@@ -0,0 +1,180 @@
+// https://forum.arduino.cc/t/esp32-c3-supermini-pinout/1189850
+// A relay controller on GPIO pins for the ESP32 supermini
+// LOW is relay energised, (led ON)
+//
+// Commands:
+// =========
+// DEMO places the target in demo cycle mode 1 relay at a time goes on/low
+// R reads all 8 output states
+// W nnnnnnnn writes all 8 outputs as either '0' or '1'
+// ON<n> number 1-32000 blink on interval
+// OFF<n> number 1-32000 blink off interval
+//
+// Replies:
+// ========
+// board responds with
+// WERR for a "Wxxxx" write error
+// WOK for a "Wnnnnnnnnn" write OK
+// Rnnnnnnnn "R" where n=states read of all 8 outputs
+// ENDD when leaving DEMO mode
+//      any command other than DEMO will stop DEMO mode
+// DEMO when acknowledging "DEMO"
+// Snnnnnnnn when in demo mode it always sends the States of the I/O pins
+//      in demo each pin gets cycled slowly!
+// ON in response to ON<n>
+// OFF in response to OFF<n>
+//
+#define LED_BUILTIN 8 // gpio 8=LED (internal next to the boot button)
+// GPIO 2,8,9 may prevent boot if pulled low during boot or flashing
+int iopins[8] = {5,6,7,10,20,21,0,1};
+
+#define MAX_SERIAL_MESSAGE 200 // for faster recovery make this as small as needed for only a few full telegrams
+bool demo_mode = false;
+bool serial_complete = false;
+String serial_message;
+int on_blink = 100;
+int off_blink = 400;
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int pin=0; pin< 8 ;pin ++)
+    pinMode(pin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  serial_message.reserve(MAX_SERIAL_MESSAGE);
+
+  Serial.begin(115200);
+  Serial.println("INIT");
+  for (int pin=0; pin< 8 ;pin ++){
+    digitalWrite(iopins[pin], HIGH);
+    pinMode(iopins[pin], OUTPUT);
+  }
+}
+
+void PollSerial(){
+  // call as often as possible
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    Serial.write('>');
+    Serial.write(inChar);
+    if (serial_message.length() < MAX_SERIAL_MESSAGE)
+      serial_message += inChar;   // discard any chars when we are full
+    if (inChar == '\n') {
+      serial_complete = true;
+    }
+  }
+}
+
+void HandleSerial_Write(String const message) {
+  // Write state
+  if (message.length()< 9){
+    Serial.println("WERR");
+    return;
+  }
+  for (int pin=0; pin< 8 ;pin ++){
+    //if (digitalRead(iopins[pin]) == HIGH)
+    if (message[1+pin]=='0')
+      digitalWrite(iopins[pin], LOW);
+    else
+      digitalWrite(iopins[pin], HIGH);
+    pinMode(iopins[pin], OUTPUT);
+  }
+  Serial.println("WOK");
+}
+
+void HandleSerial_Read(String const message) {
+  // Read state
+  String response = "R";
+  for (int pin=0; pin< 8 ;pin ++){
+    if (digitalRead(iopins[pin]) == HIGH)
+      response+='1';
+    else
+      response+='0';
+  }
+  Serial.println(response);
+}
+
+void HandleSerial_DEMO(String const message) {
+  // Read state
+  Serial.println("DEMO");
+  demo_mode = true;
+}
+
+void HandleSerial_ON(String const message) {
+  // set blink on time
+  String number = message.substring(2, message.length()-1);
+  Serial.println("ON");
+  on_blink = abs(atoi(number.c_str()));
+}
+
+void HandleSerial_OFF(String const message) {
+  // set blink off time
+  String number = message.substring(3, message.length()-1);
+  Serial.println("OFF");
+  off_blink = abs(atoi(number.c_str()));
+}
+
+
+// call only if serial_complete is true
+void HandleSerialMessages(){
+  // Grab everything up to the CR and then remove it from the buffer. 
+  // Do this until there are no more messages
+  //if (serial_complete){
+    if (demo_mode)
+    {
+      Serial.println("ENDD");  // end demo mode
+    }
+    demo_mode = false; // kick out of demo mode if we detected a message
+    // todo: call one of the above handlers
+    if (serial_message == "DEMO\n")
+      HandleSerial_DEMO(serial_message);
+    if (serial_message[0] == 'R')
+      HandleSerial_Read(serial_message);
+    if (serial_message[0] == 'W')
+      HandleSerial_Write(serial_message);
+    if (serial_message[0] == 'O')
+    {
+      if (serial_message[1]== 'N')
+        HandleSerial_ON(serial_message);
+      if (serial_message[1]== 'F')
+        HandleSerial_OFF(serial_message);
+    }
+    serial_complete = false;
+    serial_message = ""; // todo: shift out up to last <CR>
+    if (serial_message.lastIndexOf('\n') >=0)
+      serial_message = serial_message.substring(serial_message.lastIndexOf('\n'));
+    else
+      serial_message = "";
+  //}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (demo_mode) {
+    for (int pin=0; pin< 8 ;pin ++){
+        pinMode(iopins[pin], OUTPUT);
+        digitalWrite(iopins[pin], LOW);   // Turn LED on (try HIGH if inverted)
+        digitalWrite(LED_BUILTIN, HIGH); // LED off
+        PollSerial();
+        delay(1000);                     // Wait for 1 second
+        digitalWrite(iopins[pin], HIGH);   // Turn LED on (try HIGH if inverted)
+        digitalWrite(LED_BUILTIN, LOW); // LED on
+        PollSerial();
+        delay(500);                     // Blink LED
+        pinMode(iopins[pin], INPUT);
+        String statusmessage = "S00000000";
+        statusmessage[1+pin] = '1';
+        Serial.println(statusmessage);
+    }
+  }else{
+    // the non-demo mode
+    PollSerial();
+    digitalWrite(LED_BUILTIN, HIGH); // LED off
+    delay(off_blink);                     
+    digitalWrite(LED_BUILTIN, LOW); // LED on
+    delay(on_blink);                     
+  }
+  if (serial_complete) {
+    HandleSerialMessages();
+  }
+}
